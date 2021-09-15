d8d9e62419f09836fa601144b27dc0b8f09b4b45
Alperdec <49699829+Alperdec@users.noreply.github.com>
2021-09-14 18:44:45 -0700

    updates

--- src/utils/admin.js
+++ src/utils/admin.js
@@ -1,10 +1,11 @@
-const admin = require("firebase-admin");
-const serviceAccount = require("../../config.json");
+const admin = require('firebase-admin')
+const serviceAccount = require('../../config.json')
+
 admin.initializeApp({
-    credential: admin.credential.cert(serviceAccount)
-});
+  credential: admin.credential.cert(serviceAccount)
+})
 
-db = admin.firestore();
-writeBatch = db.batch();
+const db = admin.firestore()
+const writeBatch = db.batch()
 
-module.exports = { db, writeBatch };
+module.exports = { db, writeBatch }
