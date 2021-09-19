0ba0f27757cf62e63973632788d17cfb09e50d0f
Alperdec <49699829+Alperdec@users.noreply.github.com>
2021-09-19 14:03:58 -0700

    changed applicant validation schema to allow only auth0 ids as doc ids

--- src/__tests__/validate/index.test.js
+++ src/__tests__/validate/index.test.js
@@ -2,7 +2,7 @@ const { validate } = require('../../utils/utils')
 
 describe('validating input data', () => {
   test('given valid data', () => {
-    expect(validate({ applicant: '001', status: 'accepted' })).toBe(true)
+    expect(validate({ applicant: 'auth0|aaa111bbb222ccc333ddd444', status: 'accepted' })).toBe(true)
   })
   test('given no data', () => {
     expect(() => validate({})).toThrow(JSON.stringify({
@@ -11,8 +11,8 @@ describe('validating input data', () => {
     }))
   })
   test('given valid id but no status', () => {
-    expect(() => validate({ applicant: '001' })).toThrow(JSON.stringify({
-      applicant: '001',
+    expect(() => validate({ applicant: 'auth0|aaa111bbb222ccc333ddd444' })).toThrow(JSON.stringify({
+      applicant: 'auth0|aaa111bbb222ccc333ddd444',
       message: 'application status is not provided!'
     }))
   })
@@ -25,26 +25,36 @@ describe('validating input data', () => {
   test('given invalid ids', () => {
     expect(() => validate({ applicant: 'ABC', status: 'accepted' })).toThrow(JSON.stringify({
       applicant: 'ABC',
-      message: 'applicant id is invalid! Valid schema is /^[0-9]+$/'
+      message: 'applicant id is invalid! Valid schema is /^auth0|[a-z0-9]+$/'
+    }))
+    expect(() => validate({ applicant: 'auth0|', status: 'accepted' })).toThrow(JSON.stringify({
+      applicant: 'auth0|',
+      message: 'applicant id is invalid! Valid schema is /^auth0|[a-z0-9]+$/'
     }))
     expect(() => validate({ applicant: '%$^&((#)!_@!#*&T^', status: 'accepted' })).toThrow(JSON.stringify({
       applicant: '%$^&((#)!_@!#*&T^',
-      message: 'applicant id is invalid! Valid schema is /^[0-9]+$/'
+      message: 'applicant id is invalid! Valid schema is /^auth0|[a-z0-9]+$/'
+    }))
+  })
+  test('given id is too long', () => {
+    expect(() => validate({ applicant: 'auth0|aaa111bbb222ccc333ddd444aaa111bbb222ccc333ddd444' })).toThrow(JSON.stringify({
+      applicant: 'auth0|aaa111bbb222ccc333ddd444aaa111bbb222ccc333ddd444',
+      message: 'applicant id is invalid! The provided id exceeds the maximum allotted char count of 30!'
     }))
   })
   test('given valid id but invalid status', () => {
-    expect(() => validate({ applicant: '1234', status: 'poop' })).toThrow(JSON.stringify({
-      applicant: '1234',
+    expect(() => validate({ applicant: 'auth0|aaa111bbb222ccc333ddd444', status: 'poop' })).toThrow(JSON.stringify({
+      applicant: 'auth0|aaa111bbb222ccc333ddd444',
       message: 'application status is invalid!',
       status: 'poop'
     }))
-    expect(() => validate({ applicant: '1234', status: 'Cruzhacks rocks!' })).toThrow(JSON.stringify({
-      applicant: '1234',
+    expect(() => validate({ applicant: 'auth0|aaa111bbb222ccc333ddd444', status: 'Cruzhacks rocks!' })).toThrow(JSON.stringify({
+      applicant: 'auth0|aaa111bbb222ccc333ddd444',
       message: 'application status is invalid!',
       status: 'Cruzhacks rocks!'
     }))
-    expect(() => validate({ applicant: '1234', status: 'Accepted' })).toThrow(JSON.stringify({
-      applicant: '1234',
+    expect(() => validate({ applicant: 'auth0|aaa111bbb222ccc333ddd444', status: 'Accepted' })).toThrow(JSON.stringify({
+      applicant: 'auth0|aaa111bbb222ccc333ddd444',
       message: 'application status is invalid!',
       status: 'Accepted'
     }))
--- src/utils/utils.js
+++ src/utils/utils.js
@@ -13,10 +13,16 @@ const validate = (applicant) => {
     const docId = applicant.applicant
 
     // test applicant id for valid schema
-    if (!/^[0-9]+$/.test(applicant.applicant)) {
+    if (!/^auth0\|[a-z0-9]+$/.test(docId)) {
       throw JSON.stringify({
         applicant: docId,
-        message: 'applicant id is invalid! Valid schema is /^[0-9]+$/'
+        message: 'applicant id is invalid! Valid schema is /^auth0|[a-z0-9]+$/'
+      })
+    }
+    if (docId.length > 30) {
+      throw JSON.stringify({
+        applicant: docId,
+        message: 'applicant id is invalid! The provided id exceeds the maximum allotted char count of 30!'
       })
     }
     if (Object.keys(applicant).includes('status')) {
